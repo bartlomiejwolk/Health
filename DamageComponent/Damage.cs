4437c8adadaa27a10488d9719244f9f8f87b4890
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-05-27 14:05:37 +0200

    Create `Damage` component

---
+++ DamageComponent/Damage.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace HealthEx.DamageComponent {
+
+    public sealed class Damage : MonoBehaviour {
+
+        #region CONSTANTS
+
+        public const string Version = "v0.1.0";
+        public const string Extension = "Health";
+
+        #endregion
+
+        #region DELEGATES
+        #endregion
+
+        #region EVENTS
+        #endregion
+
+        #region FIELDS
+
+#pragma warning disable 0414
+        /// <summary>
+        /// Allows identify component in the scene file when reading it with
+        /// text editor.
+        /// </summary>
+        [SerializeField]
+        private string componentName = "Damage";
+#pragma warning restore0414
+
+        #endregion
+
+        #region INSPECTOR FIELDS
+
+        [SerializeField]
+        private string description = "Description";
+
+        #endregion
+
+        #region PROPERTIES
+
+        /// <summary>
+        /// Optional text to describe purpose of this instance of the component.
+        /// </summary>
+        public string Description {
+            get { return description; }
+            set { description = value; }
+        }
+
+        #endregion
+
+        #region UNITY MESSAGES
+
+        private void Awake() { }
+
+        private void FixedUpdate() { }
+
+        private void LateUpdate() { }
+
+        private void OnEnable() { }
+
+        private void Reset() { }
+
+        private void Start() { }
+
+        private void Update() { }
+
+        private void OnValidate() { }
+
+        #endregion
+
+        #region EVENT INVOCATORS
+        #endregion
+
+        #region EVENT HANDLERS
+        #endregion
+
+        #region METHODS
+        #endregion
+
+    }
+
+}
---
+++ DamageComponent/Editor/DamageEditor.cs
@@ -0,0 +1,71 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace HealthEx.DamageComponent {
+
+    [CustomEditor(typeof(Damage))]
+    [CanEditMultipleObjects]
+    public sealed class DamageEditor : Editor {
+        #region FIELDS
+
+        private Damage Script { get; set; }
+
+        #endregion FIELDS
+
+        #region SERIALIZED PROPERTIES
+
+        private SerializedProperty description;
+
+        #endregion SERIALIZED PROPERTIES
+
+        #region UNITY MESSAGES
+
+        public override void OnInspectorGUI() {
+            serializedObject.Update();
+
+            DrawVersionLabel();
+            DrawDescriptionTextArea();
+
+            EditorGUILayout.Space();
+
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        private void OnEnable() {
+            Script = (Damage)target;
+
+            description = serializedObject.FindProperty("description");
+        }
+
+        #endregion UNITY MESSAGES
+
+        #region INSPECTOR CONTROLS
+
+        private void DrawVersionLabel() {
+            EditorGUILayout.LabelField(
+                string.Format(
+                    "{0} ({1})",
+                    Damage.Version,
+                    Damage.Extension));
+        }
+
+        private void DrawDescriptionTextArea() {
+            description.stringValue = EditorGUILayout.TextArea(
+                description.stringValue);
+        }
+
+        #endregion INSPECTOR
+
+        #region METHODS
+
+        [MenuItem("Component/Health/Damage")]
+        private static void AddEntryToComponentMenu() {
+            if (Selection.activeGameObject != null) {
+                Selection.activeGameObject.AddComponent(typeof(Damage));
+            }
+        }
+
+        #endregion METHODS
+    }
+
+}
